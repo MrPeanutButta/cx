023802e3e52668fcfc52ae06e8de242747bc7827
Aaron Hebert <aaron.hebert@gmail.com>
2014-04-30 16:43:02 -0700

    Completed namespace, to_str, to_int

--- examples/exec_test.cxx
+++ examples/exec_test.cxx
@@ -1,37 +1,21 @@
 #include io
-#include char
-
-class bool {
-    bool is_false() {		
-        return (this == false);
-    }
-}
-
-class char {
-    char do_di_do(int a) {
-        return (this + a);
-    }
-}
-
-namespace ding {
-    void t(char &ch) {
-        io::puts("ding::t(" + ch.to_str + ") called");
-    }
-}
 
 /** @param void
  */
 int main () {
 
-    char c = 'F';
-    bool a = false;
-    bool b = true;
-   
-    ding::t(c);
-	
-    io::puts(c.do_di_do(10).to_str);
-    io::puts(a.is_false.to_str);
-    io::puts(b.is_false.to_str);
-
+    //char *a_string = "100";
+    bool *b = {true,false,false,true,true};
+    int *a = {0,1,2,3,4};
+    char *c = "some long ass string to_str splat";
+    float *d = {1.1,2.2,3.3,4.4,5.5};
+    
+    //int a_int = ;
+    
+    io::puts(a.to_str);
+    io::puts(b.to_str);
+    io::puts(c.to_str);
+    io::puts(d.to_str);
+    
     return 0;
 }
